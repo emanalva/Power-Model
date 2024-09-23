97c172b9c8129484b69d989111283271f7b73316
Emanuel Alvarez <alvarez-emanuel@outlook.com>
2024-09-23 01:33:46 -0400

    bug fix build:css and watch:css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    './templates/**/*.html',   // This will scan your HTML files in 'templates' directory
+    './static/js/**/*.js',     // This will scan your JS files in 'static/js' directory
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
